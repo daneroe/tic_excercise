45e6e1c32b297375a697b879eae6c6f026c864b0
Dane Roe <daneroe@gmail.com>
2022-11-05 17:15:09 +1030

    Client side shell done!

--- savings-calculator/ClientApp/src/components/Home.js
+++ savings-calculator/ClientApp/src/components/Home.js
@@ -1,26 +1,67 @@
 import React, { Component } from 'react';
+import { Row } from 'reactstrap';
 
 export class Home extends Component {
   static displayName = Home.name;
 
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      customerRate: 0.468,
+      borrowAmount: 6000000,
+      savingsObject: null, 
+      loading: false };
+  }
+
+  handleRateChange = (event) => this.setState({ customerRate : event.target.value })
+  handleAmountChange = (event) => this.setState({ borrowAmount : event.target.value })
+
+  render() {
+
+    let load = this.state.loading
+    ? <p><em>Loading...</em></p>
+    : <p><em>Done...</em></p>;
+
+    let savings = this.state.savingsObject == null
+    ? <p><em>Loading...</em></p>
+    : <p><em>Done...</em></p>;
+
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        {load}
+        <Row>
+          <h3>Current Rate</h3>
+          <input id='rate' type='number' value={this.state.customerRate} onChange= {this.handleRateChange} ></input>
+        </Row>
+        <Row>
+          <h3>Borrowing Amount</h3>
+          <input id='amount' type='number' value={this.state.borrowAmount} onChange={this.handleAmountChange} ></input>
+        </Row>
+        <Row> 
+          <button onClick={() => this.getSavings() }>Submit</button>
+        </Row>
+        {savings}
       </div>
-    );
+    )
+
+
+
+    // return (
+    //   <div>
+    //     <h1 id="tabelLabel" >Weather forecast</h1>
+    //     <p>This component demonstrates fetching data from the server.</p>
+    //     {contents}
+    //   </div>
+    // );
+  }
+
+  async getSavings() {
+    const rate = this.state.customerRate;
+    const amount = this.state.borrowAmount;
+    this.setState({ loading: true })    
+    const response = await fetch(`https://localhost:5001/SavingsCalculator?CustomerRate=${rate}&BorrowingAmount=${amount}`);
+    const data = await response.json();
+    console.log("Returning", JSON.parse(data.content))
+    this.setState({ savingsObject: JSON.parse(data.content), loading: false });
   }
 }
